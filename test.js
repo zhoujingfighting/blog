0f3ba9026db2d83f0dfe43ba936f013203abaa22
zhoujing <jing.zhou@terapines.com>
2021-12-19 22:49:31 +0800

    Add `Nodejs global object`

--- test.js
+++ test.js
@@ -1,4 +1,17 @@
+const eventEmmiter = require('events');
 
-const a = 1;
-const b = 2;
-console.log(a + b);
+const myEvent = new eventEmmiter();
+
+myEvent.on('event1', () => {
+    console.log('hhhhhh');
+})
+
+myEvent.emit('event1')
+//console.log(Object.prototype.toString.call(global))
+//工作目录
+
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', (arg) => {
+    let chunk = process.stdin.read()
+    process.stdout.write(chunk)
+})
