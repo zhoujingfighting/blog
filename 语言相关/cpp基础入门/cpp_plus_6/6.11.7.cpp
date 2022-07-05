ffc4e2b46756917c3dc0553023f325a640383ba2
zhoujing <jing.zhou@terapines.com>
2022-07-05 09:28:51 +0800

    [语言相关]c++_练习题6.11-7.cpp

--- 语言相关/cpp基础入门/cpp_plus_6/6.11.7.cpp
+++ 语言相关/cpp基础入门/cpp_plus_6/6.11.7.cpp
@@ -10,17 +10,53 @@
  */
 #include <iostream>
 #include <cctype>
-using namespace std;
+#include <string>
 int main(void)
 {
-
-    char *input;
-    while (cin >> input)
+    using namespace std;
+    char input;
+    cout << "Enter words (q to quit): " << endl;
+    int owl = 0; //元音
+    int fal = 0; //辅音
+    string value = "";
+    while (cin.get(input))
     {
-        if (input == "q")
+        if (input == 'q')
         {
-            std::cout << "End" << endl;
+            break;
+            cout << "Break" << endl;
         }
+        if (input == '\n')
+        {
+            cin.get();
+            continue;
+        }
+        else if (input == ' ')
+        {
+            if(isalpha(value[0])){//首字母是字符
+                switch (tolower(value[0]))
+                {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    owl++;
+                    break;
+                
+                default:
+                    fal++;
+                }
+            }
+            value = "";
+        }
+        else
+        {
+            value.push_back(input);
+        }
+        cout << input << endl;
     }
+    cout << owl <<" words begining with vowels" << endl;
+    cout << fal <<" words begining with consonants" << endl;
     return 0;
 }
