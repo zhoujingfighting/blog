4c67ba939588f887e4c988d691136edf8c4e476a
zhoujing <jing.zhou@terapines.com>
2022-07-19 18:14:20 +0800

    [语言相关]c++_prime第八章update1

--- 语言相关/cpp基础入门/cpp_plus_8/template.cpp
+++ 语言相关/cpp基础入门/cpp_plus_8/template.cpp
@@ -10,14 +10,23 @@
  */
 
 #include <iostream>
+using namespace std;
 template <typename T>
-
-void swap(T &x, T &y){ //
+void swap(T &x, T &y) { //
     T temp = x;
     x = y;
     y = temp;
 }
-
+template <class T, class U = char>
+class Test{
+public:        
+    Test():index1(12), index2(123.00){
+        //构造函数
+       std::cout << "Being created!!" << endl;
+    };
+    T index1;
+    U index2;
+};
 int main(void){
     using namespace std;
     int a = 10;
@@ -25,5 +34,10 @@ int main(void){
     ::swap(a,b); //不加冒号会提示出swap is ambiguous的错误
     std::cout << a << endl;
     std::cout << b << endl;
+
+    //test class template
+    Test<int, double> Index;
+    std::cout << Index.index1 << endl;
+    std::cout << Index.index2 << endl;
     return 0;
 }
