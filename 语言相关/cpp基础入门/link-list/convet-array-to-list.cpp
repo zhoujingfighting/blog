83633ac0f1d92823ba50666d2328abc46d35aa6e
zhoujingfighting <254644528@qq.com>
2022-11-14 22:27:04 +0800

    Convert array to list node

--- 语言相关/cpp基础入门/link-list/convet-array-to-list.cpp
+++ 语言相关/cpp基础入门/link-list/convet-array-to-list.cpp
@@ -7,21 +7,12 @@
 #include <iostream>
 using namespace std;
 
-vector<int> toVector() {
-    // TODO: fix this
-    int input[3] = {1,2,3};
-    vector<int> Input;
-    for(int i = 0 ; i < 3 ; i ++){
-        Input.push_back(input[i]);
-    }
-    return Input;
-}
-
 ListNode *convertArrayToListNode() {
-    vector<int> Input(toVector());
+    // FIXME: need to be changed
+    vector<int> Array = {1,2,3,4,5,6,7,8};
     ListNode *Dummy = new ListNode(0);
     ListNode *Head = Dummy;
-    for(auto Node : Input) {
+    for(auto Node : Array) {
         Head->next = new ListNode(Node);
         Head = Head->next;
     }
