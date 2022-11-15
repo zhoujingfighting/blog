f6dab1e701dc98b391bd0669e18cb599a47cb4dc
zhoujing <jing.zhou@terapines.com>
2022-11-15 09:11:04 +0800

    Bug fix

--- 语言相关/cpp基础入门/link-list/useStackToReverseList.cpp
+++ 语言相关/cpp基础入门/link-list/useStackToReverseList.cpp
@@ -12,32 +12,24 @@
 #include "list-node.h"
 #include <stack>
 
-ListNode *convertArrayToListNode(std::vector<int> Array) {
-    ListNode *Dummy = new ListNode(0);
-    ListNode *Head = Dummy;
-    for(auto Node : Array) {
-        Head->next = new ListNode(Node);
-        Head = Head->next;
-    }
-    return Dummy->next;
-}
+extern ListNode *convertArrayToListNode(std::vector<int> Array);
 
 /// @brief Can use recursive to reverse, but that way is ineffective,so We use this function
 /// @param InputList The to be reversed ListNode
 /// @return a new ListNode
 ListNode *reverseListByStack(std::vector<int> Input){
     //std::vector<int> Input = {1,2,3,4,5,6,7,8,9};  // The tested array
-    std::stack<ListNode *> Stack;
+    std::stack<int> Stack;
     ListNode *InputList = convertArrayToListNode(Input);
     while(InputList) {
-        Stack.push(InputList);
+        Stack.push(InputList->val);
         InputList = InputList->next;
     }
     // Now, we need to pop Stack to get new ListNode
     ListNode *Dummy = new ListNode(0);
     ListNode *Head = Dummy;
     while(!Stack.empty()) {
-        Dummy->next = Stack.top();
+        Dummy->next = new ListNode(Stack.top());
         Stack.pop();
         Dummy = Dummy->next;
     }
