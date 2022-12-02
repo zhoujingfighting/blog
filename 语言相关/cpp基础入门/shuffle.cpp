3ab4c5bd7c6515761e66edb659a81e0581f0e6ca
zhoujing <jing.zhou@terapines.com>
2022-12-02 11:03:55 +0800

    [Cpp] Initialize shuffle algorithm imp

---
+++ 语言相关/cpp基础入门/shuffle.cpp
@@ -0,0 +1,24 @@
+
+/// 每次随机选取一个数，然后将该数与数组中最后(或最前)的元素相交换(如果随机选中的是最后/最前的元素，则相当于没有发生交换)；
+/// 然后缩小选取数组的范围，去掉最后的元素,即之前随机抽取出的数。重复上面的过程，直到剩余数组的大小为1，即只有一个元素时结束。
+
+/// Test shuffle algorithm
+/// @link https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+/// @brief -- To shuffle an array a of n elements (indices 0..n-1): \
+/// for i from n−1 downto 1 do
+/// j ← random integer such that 0 ≤ j ≤ i
+/// exchange a[j] and a[i]
+void shuffleAlgorithm(char* input) {
+
+}
+
+/// Get a random deck from card decks
+char *getRandomDeck() {
+    char test[54];
+    return test; 
+}
+
+
+int main() {
+    return  0;
+}
